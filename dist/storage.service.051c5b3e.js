0cfee590042a04dbc94b24f679451943da7a4197
AndreaStefanutti99 <a.stefanutti@nesea.it>
2024-10-10 15:05:31 +0200

    Add parcel-bundler

--- dist/storage.service.051c5b3e.js
+++ dist/storage.service.051c5b3e.js
@@ -158,7 +158,7 @@ function initGamesValue() {
 }
 function getValueFromKey() {}
 function saveDataFromKey() {}
-},{}],"../../../../Users/AntonioDECUSATI/AppData/Roaming/nvm/v14.17.6/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"../../../AppData/Roaming/nvm/v14.17.6/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -183,7 +183,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56610" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58246" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
@@ -327,5 +327,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../Users/AntonioDECUSATI/AppData/Roaming/nvm/v14.17.6/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","services/storage.service.js"], null)
+},{}]},{},["../../../AppData/Roaming/nvm/v14.17.6/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","services/storage.service.js"], null)
 //# sourceMappingURL=/storage.service.051c5b3e.js.map
